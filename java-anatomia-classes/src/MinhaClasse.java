3d58f25a1f239260d9e68d53b5cdbce350cab055
Caio Eduardo <caiomizohata@gmail.com>
2024-04-30 18:22:08 -0300

    Novo arquivo java e pasta adicionados

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,14 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         
-        //Conteúdo
+        String primeiroNome = "Kyle";
+        String segundoNome = "Crane";
 
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do primeiro método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
