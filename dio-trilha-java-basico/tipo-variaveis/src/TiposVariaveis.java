ad9405367ad93b71f0cb1431f2a3bfab988a5101
Caio Hisamatsu <caiomizohata@gmail.com>
2024-05-01 21:45:23 -0300

    Conteúdo de tipos de variáveis

---
+++ dio-trilha-java-basico/tipo-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+        String meuNome = "Nome";
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // Declaração de variável constante (O valor da variável não pode ser alterado após ser declarado)
+        
+        final int numero = 5;
+        
+        
+        
+    }
+}
